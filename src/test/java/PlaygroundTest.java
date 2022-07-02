9925634f474cd48f4c939b5a11329ad6bb04734f
Adelina <misssluv@yandex.ru>
2022-07-02 17:40:26 +0300

    add waiter

--- src/test/java/PlaygroundTest.java
+++ src/test/java/PlaygroundTest.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class PlaygroundTest {
 
     @Test
     public void firstTest(){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get("https://1lib.tw");
         driver.findElement(By.id("searchFieldx")).sendKeys("architecture of happiness");
