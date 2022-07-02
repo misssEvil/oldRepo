85b016b7f594d9fc15f339ffadc09a462368e4bb
Adelina <misssluv@yandex.ru>
2022-07-02 17:54:44 +0300

    change driver back

--- src/test/java/PlaygroundTest.java
+++ src/test/java/PlaygroundTest.java
@@ -2,8 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.opera.OperaDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,9 +11,8 @@ public class PlaygroundTest {
 
     @Test
     public void firstTest(){
-        WebDriverManager.operadriver().setup();
-        WebDriver driver = new OperaDriver();
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
