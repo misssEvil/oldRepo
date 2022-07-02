39f9a3ef454292651549068b4d8eb5b24e2b2437
Adelina <misssluv@yandex.ru>
2022-07-02 23:29:04 +0300

    add negative test

--- src/test/java/SearchBookTest.java
+++ src/test/java/SearchBookTest.java
@@ -6,13 +6,24 @@ import runner.BaseTest;
 public class SearchBookTest extends BaseTest {
 
     @Test
-    public void findBookTest(){
+    public void findBookPositiveTest(){
 
             getDriver().get("https://1lib.tw");
             getDriver().findElement(By.id("searchFieldx")).sendKeys("architecture of happiness");
             getDriver().findElement(By.className("inner")).click();
 
             Assert.assertEquals(getDriver().findElement(By.linkText("The Architecture of Happiness")).getText(), "The Architecture of Happiness");
+
+    }
+
+    @Test
+    public void findBookNegativeTest(){
+
+        getDriver().get("https://1lib.tw");
+        getDriver().findElement(By.id("searchFieldx")).sendKeys("pride and perjustrice");
+        getDriver().findElement(By.className("inner")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.linkText("Pride and Perjustrice")).getText(), "Pride and Perjustrice");
 
     }
 }
